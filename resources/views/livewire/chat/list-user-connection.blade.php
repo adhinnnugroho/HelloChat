db5fef4c0057a79d2f896e86e0f243dbf5b17f65
adhinnnugroho <adhinnnugroho@gmail.com>
2024-02-14 16:11:10 +0700

    change function to create voice-note feature to external file js

--- resources/views/livewire/chat/list-user-connection.blade.php
+++ resources/views/livewire/chat/list-user-connection.blade.php
@@ -41,72 +41,8 @@
     <!-- end message -->
 
     @push('scripts')
-        <script>
-            function voiceNote() {
-                return {
-                    mediaRecorder: null,
-                    audioChunks: [],
-                    isRecording: false,
-                    audioURL: null,
-                    showMicNotFound: false,
-
-                    startRecording() {
-                        navigator.mediaDevices.getUserMedia({
-                                audio: true
-                            })
-                            .then(stream => {
-                                this.mediaRecorder = new MediaRecorder(stream);
-
-                                this.mediaRecorder.ondataavailable = event => {
-                                    if (event.data.size > 0) {
-                                        this.audioChunks.push(event.data);
-                                    }
-                                };
-
-                                this.mediaRecorder.onstop = () => {
-                                    const blob = new Blob(this.audioChunks, {
-                                        type: 'audio/wav'
-                                    });
-                                    // this.audioURL = URL.createObjectURL(blob);
-                                };
-
-                                this.mediaRecorder.start();
-                                this.isRecording = true;
-                            })
-                            .catch(error => {
-                                console.error('Error accessing microphone:', error);
-                                this.showMicNotFound = true;
-                            });
-                    },
-
-                    stopRecording() {
-                        if (this.isRecording) {
-                            this.mediaRecorder.stop();
-
-                            const blob = new Blob(this.audioChunks, {
-                                type: 'audio/wav'
-                            });
-
-                            let formData = new FormData();
-                            formData.append('audio', blob);
-                            let formDataObject = {};
-                            formData.forEach(function(value, key) {
-                                formDataObject[key] = value;
-                            });
-                            Livewire.dispatch('savedAudio', {
-                                audio: formDataObject
-                            });
-                            this.isRecording = false;
-                        }
-                    },
-
-                    hideMicNotFound() {
-                        this.showMicNotFound = false;
-                    },
-                };
-            }
-        </script>
-        <script src="{{ asset('/assets/js/textarea-animations.js') }}"></script>
+        <script src="{{ asset('/assets/js/voice-note.js') }}" />
+        <script src="{{ asset('/assets/js/textarea-animations.js') }}" />
         <script>
             function submitForm() {
                 Livewire.dispatch('savedChat')

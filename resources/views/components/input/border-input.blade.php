fcb694ee1970af7c12bec7187d1352f9e54290f0
adhinnnugroho <adhinnnugroho@gmail.com>
2024-03-08 21:08:35 +0700

    fix: error in border input

--- resources/views/components/input/border-input.blade.php
+++ resources/views/components/input/border-input.blade.php
@@ -1,6 +1,6 @@
 <div class="py-4 px-2">
     @if (!is_null($label))
-        <label for="" class="text-base block mb-2 ml-2">{{ $label }}</label>
+        <label class="text-base block mb-2 ml-2">{{ $label }}</label>
     @endif
     <input class="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
         x-bind:class="{ 'bg-black text-white border-gray-600': $store.darkMode.on }" {{ $attributes }} />
